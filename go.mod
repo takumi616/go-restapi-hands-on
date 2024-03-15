8acc0b587a87890deb9f25ca49ee0c55c1840b46
takumi616 <ohohyeah321@icloud.com>
2024-03-16 01:30:55 +0900

    Edit mux.go file to add chi package.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/caarlos0/env/v10 v10.0.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
--- mux.go
+++ mux.go
@@ -1,13 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/takumi616/go-restapi-hands-on/handler"
+	"github.com/takumi616/go-restapi-hands-on/store"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
+
 	//An endpoint to check if http server is running correctly
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	//Create validator
+	v := validator.New()
+
+	//Register http handler
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
